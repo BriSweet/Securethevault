9db5dafbac8233de6fb4819c983eb6c0d118cc49
BriSweet <brianasweet12@gmail.com>
2025-07-23 10:26:05 -0600

    Change console

---
+++ index.js
@@ -0,0 +1,16 @@
+var code1
+var code1 = 7 + 3
+console.log(code1 + 3);
+
+var code2 = 100 / 2 - 10
+
+var code3
+var code3 = 40 - (1 % 2)
+console.log(code3);
+
+var message = "The Vault has been secured! The combination is ";
+
+var codeA = code1 + code2 + code3;
+
+var codeB = (`${code1} + ${code2} + ${code3} = ${codeA}`);
+console.log(message + codeB);
