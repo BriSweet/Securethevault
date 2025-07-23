a44e645e5a8491dab1b356b85b2ce0735f1c44ba
BriSweet <brianasweet12@gmail.com>
2025-07-23 10:37:32 -0600

    Fixed console issues again

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-var code1
+var code1 = 10
 var code1 = 7 + 3
-console.log(code1 + 3);
 
 var code2 = 100 / 2 - 10
 
@@ -11,6 +10,7 @@ console.log(code3);
 var message = "The Vault has been secured! The combination is ";
 
 var codeA = code1 + code2 + code3;
+console.log(codeA);
 
-var codeB = (`${code1} - ${code2} - ${code3} - ${codeA}`);
-console.log(message + codeB);
+var codeB = (`${code1} - ${code2} - ${code3}`);
+console.log(message + codeB + " - " + codeA);
